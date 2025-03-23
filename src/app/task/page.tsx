8d59f10f7e664f803e1fa90b89986bf3d0ea8499
Shogo Wada <aiz4338150@gmail.com>
2025-03-23 22:17:40 +0900

    feat: データ取得検証・サーバーコンポーネントでORMで直接取得

--- src/app/task/page.tsx
+++ src/app/task/page.tsx
@@ -1,8 +1,24 @@
 import PageTitle from '@/components/PageTitle';
-import Tasks from '@/components/Tasks';
+// import Tasks from '@/components/Tasks';
+import { createClient } from '@/utils/supabase/server';
+import { PrismaClient } from '@prisma/client';
 
 // タスク一覧画面
-export default function TasksPage() {
+export default async function TasksPage() {
+  const supabase = await createClient();
+  const { data: userData, error } = await supabase.auth.getUser();
+  if (error) {
+    console.error(error.message);
+    return [];
+  }
+
+  const prisma = new PrismaClient();
+  const tasks = await prisma.task.findMany({
+    where: {
+      authorId: userData.user.id,
+    },
+  });
+
   return (
     <>
       <PageTitle title="タスク一覧" />
@@ -13,7 +29,28 @@ export default function TasksPage() {
           <p className="w-1/10">ステータス</p>
           <p className="w-1/10">期限</p>
         </div>
-        <Tasks />
+        {/* <Tasks /> */}
+        {tasks.length > 0 &&
+          tasks.map((task) => (
+            <div
+              key={task.id}
+              className="flex flex-row gap-3 hover:cursor-pointer border-b border-blue-800"
+              // onClick={() => router.push(`/task/${task.id}`)}
+            >
+              <div className="w-1/10 group">
+                <p className="truncate group-hover:whitespace-normal group-hover:opacity-80">{task.title}</p>
+              </div>
+              <div className="w-2/3 group">
+                <p className="truncate group-hover:whitespace-normal group-hover:opacity-80">{task.description}</p>
+              </div>
+              <div className="w-1/10 group">
+                <p className="truncate group-hover:whitespace-normal">{task.status}</p>
+              </div>
+              <div className="w-1/10 group">
+                <p className="truncate group-hover:whitespace-normal">{task.deadline}</p>
+              </div>
+            </div>
+          ))}
       </div>
     </>
   );
