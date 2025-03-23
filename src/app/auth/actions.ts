65a814a9444da07cab99383e21c8097f38150620
Shogo Wada <aiz4338150@gmail.com>
2025-03-24 00:34:08 +0900

    fix: コメント追加

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -5,11 +5,10 @@ import { redirect } from 'next/navigation';
 
 import { createClient } from '@/utils/supabase/server';
 
+/** サインイン処理 */
 export async function signin(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
@@ -25,11 +24,10 @@ export async function signin(formData: FormData) {
   redirect('/task');
 }
 
+/** サインアップ処理 */
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
