9a7a2116dc0ba62f346db7b21d9fb302db8f9c5c
Shogo Wada <aiz4338150@gmail.com>
2025-03-24 00:26:13 +0900

    feat: サインアップ機能の調整

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -1,47 +1,47 @@
-'use server'
+'use server';
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from '@/utils/supabase/server';
 
 export async function signin(formData: FormData) {
-  const supabase = await createClient()
+  const supabase = await createClient();
 
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
-  }
+  };
 
-  const { error } = await supabase.auth.signInWithPassword(data)
+  const { error } = await supabase.auth.signInWithPassword(data);
 
   if (error) {
-    redirect('/error')
+    redirect('/error');
   }
 
-  revalidatePath('/', 'layout')
-  redirect('/task')
+  revalidatePath('/', 'layout');
+  redirect('/task');
 }
 
 export async function signup(formData: FormData) {
-  const supabase = await createClient()
+  const supabase = await createClient();
 
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
     email: formData.get('email') as string,
     password: formData.get('password') as string,
-  }
+  };
 
-  const { error } = await supabase.auth.signUp(data)
+  const { error } = await supabase.auth.signUp(data);
   console.error(error);
 
   if (error) {
-    redirect('/error')
+    redirect('/error');
   }
 
-  revalidatePath('/', 'layout')
-  redirect('/task')
+  revalidatePath('/', 'layout');
+  redirect('/auth/signup/before-confirm');
 }
---
+++ src/app/auth/signup/before-confirm/page.tsx
@@ -0,0 +1,10 @@
+import PageTitle from "@/components/PageTitle";
+
+export default function BeforeConfirmPage() {
+  return (
+    <>
+      <PageTitle title="メール送信完了" />
+      <p>メールを送信しました。メール内のリンクをクリックして登録を完了してください。</p>
+    </>
+  );
+}
